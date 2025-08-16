0cadfbb527de70debc96e54187f54004524550ce
dwdwsdwd330 <dwdwsdwd330@gmail.com>
2025-08-16 21:29:48 +0530

    Create missing main.tsx entry point

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-center py-8">
+        Anti-Christ Dark Gothic Ecommerce Website
+      </h1>
+      <p className="text-center text-gray-300">
+        Application is now running successfully
+      </p>
+    </div>
+  )
+}
+
+export default App
